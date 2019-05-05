29b32ce3f70b79459ae84cc807382409ed3a8320
Bobby Ly <49233040+bobbyly@users.noreply.github.com>
2019-05-05 11:13:37 +1000

    animation orback update

--- testing/C_LOOP.js
+++ testing/C_LOOP.js
@@ -1,82 +1,82 @@
 var doc = window.document,
-  context = doc.querySelector('.js-loop'),
-  clones = context.querySelectorAll('.is-clone'),
-  disableScroll = false,
-  scrollHeight = 0,
-  scrollPos = 0,
-  clonesHeight = 0,
-  i = 0;
+context = doc.querySelector('.js-loop'),
+clones = context.querySelectorAll('.is-clone'),
+disableScroll = false,
+scrollHeight = 0,
+scrollPos = 0,
+clonesHeight = 0,
+i = 0;
 
-function getScrollPos () {
-  return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
+function getScrollPos() {
+    return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
 }
 
-function setScrollPos (pos) {
-  context.scrollTop = pos;
+function setScrollPos(pos) {
+    context.scrollTop = pos;
 }
 
-function getClonesHeight () {
-  clonesHeight = 0;
+function getClonesHeight() {
+    clonesHeight = 0;
 
-  for (i = 0; i < clones.length; i += 1) {
+    for (i = 0; i < clones.length; i += 1) {
     clonesHeight = clonesHeight + clones[i].offsetHeight;
-  }
+    }
 
-  return clonesHeight;
+    return clonesHeight;
 }
 
-function reCalc () {
-  scrollPos = getScrollPos();
-  scrollHeight = context.scrollHeight;
-  clonesHeight = getClonesHeight();
+function reCalc() {
+    scrollPos = getScrollPos();
+    scrollHeight = context.scrollHeight;
+    clonesHeight = getClonesHeight();
 
-  if (scrollPos <= 0) {
+    if (scrollPos <= 0) {
     setScrollPos(1); // Scroll 1 pixel to allow upwards scrolling
-  }
+    }
 }
 
-function scrollUpdate () {
-  if (!disableScroll) {
+function scrollUpdate() {
+    if (!disableScroll) {
     scrollPos = getScrollPos();
 
     if (clonesHeight + scrollPos >= scrollHeight) {
-      // Scroll to the top when you’ve reached the bottom
-      setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
-      disableScroll = true;
+        // Scroll to the top when you’ve reached the bottom
+        setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
+        disableScroll = true;
     } else if (scrollPos <= 0) {
-      // Scroll to the bottom when you reach the top
-      setScrollPos(scrollHeight - clonesHeight);
-      disableScroll = true;
+        // Scroll to the bottom when you reach the top
+        setScrollPos(scrollHeight - clonesHeight);
+        disableScroll = true;
     }
-  }
+    }
 
-  if (disableScroll) {
+    if (disableScroll) {
     // Disable scroll-jumping for a short time to avoid flickering
     window.setTimeout(function () {
-      disableScroll = false;
+        disableScroll = false;
     }, 40);
-  }
+    }
 }
 
-function init () {
-  reCalc();
-  
-  context.addEventListener('scroll', function () {
-    window.requestAnimationFrame(scrollUpdate);
-  }, false);
+function init() {
+    reCalc();
 
-  window.addEventListener('resize', function () {
+    context.addEventListener('scroll', function () {
+    window.requestAnimationFrame(scrollUpdate);
+    }, false);
+
+    window.addEventListener('resize', function () {
     window.requestAnimationFrame(reCalc);
-  }, false);
+    }, false);
 }
 
 if (document.readyState !== 'loading') {
-  init()
+    init();
 } else {
-  doc.addEventListener('DOMContentLoaded', init, false)
+    doc.addEventListener('DOMContentLoaded', init, false);
 }
 
 // Just for this demo: Center the middle block on page load
 window.onload = function () {
-  setScrollPos(Math.round(clones[0].getBoundingClientRect().top + getScrollPos() - (context.offsetHeight - clones[0].offsetHeight) / 2));
+    setScrollPos(Math.round(clones[0].getBoundingClientRect().top + getScrollPos() - (context.offsetHeight - clones[0].offsetHeight) / 2));
 };
